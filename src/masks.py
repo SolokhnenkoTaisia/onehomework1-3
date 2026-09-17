f8596db6f64ec0ce090fd1db71125ac3d247adda
SolokhnenkoTaisia <solokhnenko.t.a@gmail.com>
2026-09-17 14:27:00 +1000

    fix card mask format

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(card_number: str) -> str:
     """Маскирует номер банковской карты."""
-    return f"{card_number[:4]} {card_number[4:6]} ** {card_number[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
